425bfd595a071305acd3994a042f861631b4f0c9
Pablo <pablobendiksen@gmail.com>
2023-05-30 16:50:50 -0400

    fix validation_accuracies.py

--- validation_accuracies.py
+++ validation_accuracies.py
@@ -11,6 +11,6 @@ for file in csv_files:
     df_temp = pd.read_csv(os.path.join(path, file))
     df = pd.concat([df, df_temp], ignore_index=True)
 
-df = df.sort_values('Val Accuracy', ascending=False)
+df = df.sort_values('Metric (MSE)', ascending=False)
 
 df.to_csv(os.path.join(path, 'validation_accuracies.csv'), index=False)
