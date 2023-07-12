b6350e47c64741c23f1db7032d69775a522da4f2
pablobendiksen <pablobendiksen@gmail.com>
2023-07-12 14:44:15 -0400

    job metrics check

--- collect_job_metrics.py
+++ collect_job_metrics.py
@@ -5,14 +5,15 @@ import os
 
 def collect_job_metrics():
     print("collecting job metrics")
-    path = conf.output_metrics_dir
+    path = "/hpcstor6/scratch01/p/p.bendiksen001/virtual_reality/motion-similarity/job_metrics/"
     df = pd.DataFrame()
 
     csv_files = [f for f in os.listdir(path) if f.endswith('.csv')]
 
     for file in csv_files:
+        print(file)
         df_temp = pd.read_csv(os.path.join(path, file))
-        df = pd.concat([df, df_temp], ignore_index=True)
+        df = pd.concat([df,df_temp],  ignore_index=True)
 
     df = df.sort_values('Metric (MSE)', ascending=True)
 
